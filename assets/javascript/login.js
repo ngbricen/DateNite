42e2ae2f7699705d69380a915109bf2586996ea7
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2017-06-20 20:24:48 -0400

    reorganized login logic to return a user based on geolocation

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,47 +1,92 @@
 //DUNCAN IARIA
 
-//this might handle user log in
-//and geolocation gathering
-//so that the user does not have to use zip code
-//unless they are uptight about their geolocation
+//this might handle user log in and geolocation gathering
+//so that the user does not have to use zip code unless they are uptight about their geolocation
 
-//prompt the user for location data
-function getLocation() 
+//=======================
+//	Login
+//=======================
+var loginController = ( function()
 {
-    if (navigator.geolocation) 
-    {
-        navigator.geolocation.getCurrentPosition( logPosition, showError );
-    }
-    else
-    {
-    	console.log( "could not get geo location" );
-    }
-}
+	//current user
+	var currentUser;
 
-getLocation();
+	//only the stuff that outside scripts can access
+	var publicAPI = 
+	{
+		getUser: getUser,
+		getLocation: getLocation
+	}
 
-//prove that we got a location
-function logPosition( tPos )
-{
-	console.log( tPos.coords );
-}
+	//return this api to the global scope
+	return publicAPI;
 
-//log possible errors
-function showError( error ) 
-{
-    switch( error.code ) 
-    {
-        case error.PERMISSION_DENIED:
-            console.log( "User denied the request for Geolocation." );
-            break;
-        case error.POSITION_UNAVAILABLE:
-           	console.log( "Location information is unavailable." );
-            break;
-        case error.TIMEOUT:
-            console.log( "The request to get user location timed out." );
-            break;
-        case error.UNKNOWN_ERROR:
-            console.log( "An unknown error occurred." );
-            break;
-    }
-}
+	//=======================
+	//	User Object
+	//=======================
+	function User( tName, tLatitude, tLongitude ) 
+	{
+	    this.name = tName;
+	    this.latitude = tLatitude;
+	    this.longitude = tLongitude;
+	}
+
+	//return the current user ( for allowing global access ) 
+	function getUser()
+	{
+		return currentUser;
+	}
+
+	//=======================
+	//	User Position
+	//=======================
+	//prompt the user for location data
+	function getLocation() 
+	{
+	    if (navigator.geolocation) 
+	    {
+	        //first arg is success callback, second arg is failure callback
+	        navigator.geolocation.getCurrentPosition( createUser, showError );
+	    }
+	    else
+	    {
+	    	console.log( "could not get geo location" );
+	    }
+	}
+
+	//prove that we got a location
+	function createUser( tPos )
+	{  	
+	   	//create a new user with the provided location
+	    user = new User( null, tPos.coords.latitude, tPos.coords.longitude );
+	    
+	    return user;
+	   	//console.log( tPos.coords );
+	    //console.log( tPos.coords.latitude );
+	    //console.log( tPos.coords.longitude );
+	    //console.log( user );
+	}
+
+	//log possible errors
+	function showError( error ) 
+	{
+	    switch( error.code ) 
+	    {
+	        case error.PERMISSION_DENIED:
+	            console.log( "User denied the request for Geolocation." );
+	            break;
+	        case error.POSITION_UNAVAILABLE:
+	           	console.log( "Location information is unavailable." );
+	            break;
+	        case error.TIMEOUT:
+	            console.log( "The request to get user location timed out." );
+	            break;
+	        case error.UNKNOWN_ERROR:
+	            console.log( "An unknown error occurred." );
+	            break;
+	    }
+	}
+})();
+
+//prompt the user as soon as this thing fires and save their input
+user = loginController.getLocation();
